a1b44b09518f60eac738eb1c5f07ade7f9b5af26
Charlie Brown <carbonrobot@gmail.com>
2016-03-25 10:12:47 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,51 @@
+hapi-auth-opentoken
+--------------------
+
+Opentoken authentication scheme designed to work with PINGFederate Opentoken endpoints.
+
+## Usage
+
+```js
+const opentoken = {
+    register: require('hapi-auth-opentoken'),
+    options: {
+        password: 'testPassword',
+        cipherSuite: 0,
+        tokenName: 'opentoken'
+    }
+};
+
+server.register([opentoken], (err) => {
+
+    server.auth.strategy('default', 'opentoken', { validateFunc: validate });
+    server.route({ method: 'GET', path: '/', config: { auth: 'default' } });
+    
+});
+
+function validate(request, token, callback) {
+    // token contains the decrypted saml response
+    
+    callback(err, { id: '12345', name: 'Jos Sykes' });
+}
+```
+
+## Options
+
+Opentoken takes the following options
+
+- password - the token decryption shared key (default: null)
+- cipherSuite - the opentoken cipher algorithm used by the server. One of the following [0,1,2,3] (default: 0)
+- tokenName - (required) the name of the parameter in the POST request body to parse the token from
+- tokenTolerance - The amount of time (in seconds) to allow for clock skew between servers in seconds (default: 120)
+- tokenLifetime - The duration (in seconds) for which the token is valid. (default: 300)
+- tokenRenewal - The amount of time (in seconds) the token will renew itself for. (default: 12hrs)
+
+## Validation
+
+The validation function has a signature of `validate(request, token, callback)` where
+
+- request - the hapi.js request object
+- token - the decrypted opentoken saml information
+- callback - a callback function taking the following parameters
+    - err - An optional error message which gets logged to stdout, null if no error
+    - user - the user information to attach to request.auth.credentials in downstream methods
---
+++ index.js
@@ -0,0 +1,121 @@
+const Boom = require('boom'),
+    Hoek = require('hoek'),
+    OpenTokenAPI = require('opentoken').OpenTokenAPI;
+
+// default internal settings
+const internals = {
+    settings: {
+        tokenName: 'opentoken',
+        cipherSuite: 0,
+        password: null
+    }
+};
+
+/**
+ * Authenticate for this scheme [required]
+ * 
+ * @param request - Hapi Request object @see http://hapijs.com/api#request-object
+ * @param reply - reply is a callback that must be called when your authentication is complete.
+ */
+function authenticate(request, reply) {
+    // Opentoken is sent in the payload, so we initiate passthrough
+    // because the authenticate method is required by hapi.js
+    return reply.continue({ credentials: {} });
+}
+
+/**
+ * Payload authentication for this scheme. 
+ * Opentoken is sent as a parameter in the body of the request.
+ * 
+ * @param request - Hapi Request object @see http://hapijs.com/api#request-object
+ * @param reply - reply is a callback that must be called when your authentication is complete.
+ */
+function payload(request, reply) {
+
+    console.log('Request:', {
+        id: request.id,
+        info: request.info,
+        method: request.method,
+        query: request.query,
+        payload: request.payload,
+        path: request.path,
+        headers: request.headers
+    });
+
+    // check for token existence
+    const tokenName = internals.settings.tokenName;
+    if (!request.payload) {
+        return reply(Boom.unauthorized('Missing opentoken with name [' + tokenName + '] from request body.'));
+    }
+
+    // parse token
+    var token = request.payload[tokenName];
+    internals.openTokenApi.parseToken(token, (err, data) => {
+        if (err) {
+            console.log(err);
+            return reply(Boom.unauthorized('Error parsing opentoken'));
+        }
+
+        options.validate(request, token, (err, user) => {
+            if (err) {
+                return reply(err, null);
+            }
+
+            if (!user) {
+                return reply(Boom.unauthorized('User not authenticated'));
+            }
+
+            return reply.continue({ credentials: { user: data } });
+        });
+
+    });
+}
+
+/**
+ This method must return an object with at least the key authenticate. Other optional methods that can be used are payload and response.
+ * @param server
+ * @param options
+ */
+function scheme(server, options) {
+    Hoek.assert(typeof options.validate === 'function', 'options.validate must be a valid function in opentoken scheme');
+
+    return {
+        authenticate: authenticate,
+        payload: payload,
+        options: {
+            payload: true
+        }
+    };
+};
+
+/**
+ * Register the plugin with the hapi ecosystem
+ */
+exports.register = function(server, options, next) {
+    internals.settings = Hoek.applyToDefaults(internals.settings, options);
+
+    var otkOptions = {};
+    if (options.tokenLifetime) {
+        otkOptions.tokenLifetime = options.tokenLifetime;
+    }
+    if (options.tokenRenewal) {
+        otkOptions.tokenRenewal = options.tokenRenewal;
+    }
+    if (options.tokenTolerance) {
+        otkOptions.tokenTolerance = options.tokenTolerance;
+    }
+
+    // open token processor
+    internals.openTokenApi = new OpenTokenAPI(
+        internals.settings.cipherSuite,
+        internals.settings.password,
+        otkOptions);
+
+    server.auth.scheme('opentoken', scheme);
+    return next();
+};
+
+exports.register.attributes = {
+    pkg: require('./package.json')
+};
+
